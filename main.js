ed8e1c49981d681ce22ce4b1751789877cdbe9f9
ymyto <ymto.dev@proton.me>
2025-11-08 12:11:25 +0000

    reduce使った

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const array = [2, 3, 4, 5, 6, 7, 8];
-let total = 0;
-let ave = 0;
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let total = array.reduce((sum, num) => sum + num, 0);
+let ave = total / array.length;
 
-for (let i = 0; i < array.length; i++) {
-    total = total + array[i];
-}
-ave = total / array.length;
-
-console.log("合計: " + total + "\n平均: " + ave);
+console.log(`合計: ${total}\n平均: ${ave}`);
